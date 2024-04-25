d3a3d2998f333eddeacadc1a0277dbae047dd1d1
Copilot0822 <123857131+Copilot0822@users.noreply.github.com>
2024-04-25 01:50:19 -0400

    advatagekit?

--- SwerveWithPathPlanneredit/src/main/java/frc/robot/Robot.java
+++ SwerveWithPathPlanneredit/src/main/java/frc/robot/Robot.java
@@ -4,9 +4,16 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -21,6 +28,24 @@ public class Robot extends LoggedRobot {
 
   @Override
   public void robotInit() {
+    
+    Logger.recordMetadata("ProjectName", "MyProject"); // Set a metadata value
+
+if (isReal()) {
+    Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
+    Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
+    new PowerDistribution(1, ModuleType.kRev); // Enables power distribution logging
+} else {
+    setUseTiming(false); // Run as fast as possible
+    String logPath = LogFileUtil.findReplayLog(); // Pull the replay log from AdvantageScope (or prompt the user)
+    Logger.setReplaySource(new WPILOGReader(logPath)); // Read replay log
+    Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
+}
+
+// Logger.disableDeterministicTimestamps() // See "Deterministic Timestamps" in the "Understanding Data Flow" page
+Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
+
+
     m_robotContainer = new RobotContainer();
 
     m_robotContainer.drivetrain.getDaqThread().setThreadPriority(99);
@@ -31,7 +56,7 @@ public class Robot extends LoggedRobot {
 
     /**
      * This example of adding Limelight is very simple and may not be sufficient for on-field use.
-     * Users typically need to provide a standard deviation that scales with the distance to target
+   * Users typically need to provide a standard deviation that scales with the distance to target
      * and changes with number of tags available.
      *
      * This example is sufficient to show that vision integration is possible, though exact implementation
--- SwerveWithPathPlanneredit/src/main/java/frc/robot/subsystems/Intake.java
+++ SwerveWithPathPlanneredit/src/main/java/frc/robot/subsystems/Intake.java
@@ -47,6 +47,7 @@ public class Intake extends SubsystemBase {
 
   @Override
   public void periodic() {
+    
     // This method will be called once per scheduler run
   }
   public void setIntake(double speed){ // running intake on inputted speed
